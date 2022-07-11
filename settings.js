61f7d37d9f4f53077ded3c8bd7c98ed91cb6021b
reysaanliBotMd <109074724+reysaanliBotMd@users.noreply.github.com>
2022-07-11 18:38:26 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,23 +23,23 @@ global.language = english  //change indonesia to english if you don't understand
 
 
 // Other
-global.botname = "ᴅᴀɴɪʙᴏᴛᴢ ᴏғғᴄ👑" //namabot kalian
-global.ownername= "ᴅᴀɴɪ ᴏғғɪᴄᴀʟ👑" //nama kalian
-global.myweb ="https://api.Dani.xyz" //bebas asal jan hapus
+global.botname = "ReysaBoZz" //namabot kalian
+global.ownername= "ReysaSmp" //nama kalian
+global.myweb ="https://api.Resya.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg" //bebas asal jan hapus
 global.github = "https://github.com/YTDANIBOTZ" //bebas
-global.email = "danibotz691@gmail.com" //bebas
+global.email = "resyaanli249@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["6281251549899"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6281251549899" // nomor wa kalian
-global.ownernomerr = "+6281251549899" //nmr wa kalian
+global.owner = ["62895619792384"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "62895619792384" // nomor wa kalian
+global.ownernomerr = "+62895619792384" //nmr wa kalian
 global.thumbnail = "./image/d2917e648c6a030b1980d1755c66492a.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/d2917e648c6a030b1980d1755c66492a.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
-global.packname = '© ᴅᴀɴɪʙᴏᴛᴢ ᴏғғᴄ👑' //sticker wm ubah
-global.author = 'Di Buat Oleh ᴅᴀɴɪ ᴏғғɪᴄᴀʟ👑' //sticker wm ganti nama kalian
+global.packname = '© ResysaSmp' //sticker wm ubah
+global.author = 'Di Buat Oleh ResyaBotZz' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
